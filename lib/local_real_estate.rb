803f084695fe9ca355860574b8b11ef8f178e27a
Connor Gallopo <connor.gallopo@me.com>
2018-01-29 10:34:21 -0500

    Layed out the CLI and got it working through bin/console

--- lib/local_real_estate.rb
+++ lib/local_real_estate.rb
@@ -1,5 +1,14 @@
-require "local_real_estate/version"
+# require 'open-uri'
+# require 'nokogiri'
+# require 'pry'
 
-module LocalRealEstate
-  # Your code goes here...
-end
+require "local_real_estate/version"
+require "local_real_estate/cli"
+
+
+# module LocalRealEstate
+#   # Your code goes here...
+# end
+
+
+#this is working as my enviornment file
---
+++ lib/local_real_estate/cli.rb
@@ -0,0 +1,9 @@
+class LocalRealEstate::CLI
+  def call
+    greeting
+  end
+
+  def greeting
+    puts "hello"
+  end
+end
