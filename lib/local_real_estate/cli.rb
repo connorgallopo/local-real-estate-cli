00a386e94ca0187f25e4eb9d9b63b28d089c94a6
Connor Gallopo <connor.gallopo@me.com>
2018-01-29 12:42:54 -0500

    Wired up part of the CLI to a scraper class)

--- lib/local_real_estate/cli.rb
+++ lib/local_real_estate/cli.rb
@@ -15,16 +15,17 @@ class LocalRealEstate::CLI
     puts 'If you would like to see listings in a random city press 3'
     puts 'Or type "exit"'
     input = gets.strip.downcase
-      unless input == "exit"
-        case input
-        when "1"
-          puts "enter your zip"
-          zip_code = gets.strip
-        when "2"
-          #list_cities (possibly a method that lists available cities?)
-        when "3"
-          #list_cities.sample (if list cities holds an array we can sample it to randomize)
-        end
+    unless input == 'exit'
+      case input
+      when '1'
+        puts "enter your zip"
+        zip_code = gets.strip
+        LocalRealEstate::Scraper.new.get_page(zip_code)
+      when '2'
+        #list_cities (possibly a method that lists available cities?)
+      when '3'
+        #list_cities.sample (if list cities holds an array we can sample it to randomize)
       end
     end
+  end
 end
--- lib/local_real_estate/scraper.rb
+++ lib/local_real_estate/scraper.rb
@@ -1,3 +1,12 @@
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
 class LocalRealEstate::Scraper
 
+  def get_page(zip_code = "02359") #this needs to be fixed. If zip starts with zero its lost with .to_i
+    html = open("https://www.realtor.com/realestateandhomes-search/#{zip_code.to_i}")
+    doc = Nokogiri::HTML(html)
+    binding.pry
+  end
 end
