c1669d5ba475d7b841be1f0a8eeab0f8c3f88f1c
Connor Gallopo <connor.gallopo@me.com>
2018-01-30 08:34:01 -0500

    Refined the listing class to break apart the address into city, state and address

--- lib/local_real_estate/cli.rb
+++ lib/local_real_estate/cli.rb
@@ -9,7 +9,7 @@ class LocalRealEstate::CLI
   end
 
   def menu
-    puts 'Please select from one of the options below:'
+    puts 'Please select from one of the following options below:'
     puts 'If you would like to see the listings in your area by Zip Code press 1'
     puts 'If you would like to see the listings by a major city press 2'
     puts 'If you would like to see listings in a random city press 3'
--- lib/local_real_estate/listing.rb
+++ lib/local_real_estate/listing.rb
@@ -1,9 +1,9 @@
 class LocalRealEstate::Listing
 
-  attr_accessor :address, :url, :price, :bedrooms, :bathrooms, :sqft, :cars
+  attr_accessor :address, :city, :state, :url, :price, :bedrooms, :bathrooms, :sqft, :cars
   @@all = []
 
-  def initialize(address: nil, url: nil, price: nil, bedrooms: nil, bathrooms: nil, sqft: nil, cars: nil)
+  def initialize(address: nil, city: nil, state: nil, url: nil, price: nil, bedrooms: nil, bathrooms: nil, sqft: nil, cars: nil)
     @address = address
     @url = url
     @price = price
